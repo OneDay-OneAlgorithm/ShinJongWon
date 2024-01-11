eb1c912a4de9fa4ea4040235fce9aaad10975fd6
JJongW <shinjw4675@gmail.com>
2024-01-11 23:24:19 +0900

    📝 [1874] 스택 수열

---
+++ 자료구조/1874.py
@@ -0,0 +1,31 @@
+# 📝 [1874] 스택 수열
+
+count = 1
+temp = True
+stack = []
+op = []
+
+N = int(input())
+for i in range(N):
+    num = int(input())
+    # num이하 숫자까지 스택에 넣기
+    while count <= num:
+        stack.append(count)
+        op.append('+')
+        count += 1
+
+    # num이랑 스택 맨 위 숫자가 동일하다면 제거
+    if stack[-1] == num:
+        stack.pop()
+        op.append('-')
+    # 스택 수열을 만들 수 없으므로 NO
+    else:
+        temp = False
+        break
+
+# 스택 수열을 만들수 있는지 여부에 따라 출력 
+if temp == False:
+    print("NO")
+else:
+    for i in op:
+        print(i)

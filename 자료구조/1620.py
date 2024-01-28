f489c418b481e4ecfd2ca86cd9ab4a2d9ceb33d4
JJongW <shinjw4675@gmail.com>
2024-01-28 23:54:50 +0900

    📝 [1620] 나는야 포켓몬 마스터 이다솜

---
+++ 자료구조/1620.py
@@ -0,0 +1,20 @@
+# 📝 [1620] 나는야 포켓몬 마스터 이다솜
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+dict = {}
+
+for i in range(1, n + 1):
+    a = input().rstrip()
+    dict[i] = a
+    dict[a] = i
+
+for i in range(m):
+    quest = input().rstrip()
+    if quest.isdigit():
+        print(dict[int(quest)])
+    else:
+        print(dict[quest])

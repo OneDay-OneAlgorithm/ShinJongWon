d71417e0cd7da06d2b0d3ca332d2f00ab6b82d34
JJongW <shinjw4675@gmail.com>
2024-08-01 22:04:53 +0900

    📝 [13458] 시험 감독

---
+++ 수학/13458.py
@@ -0,0 +1,16 @@
+# 📝 [13458] 시험 감독
+
+n=int(input())
+n_cnt=list(map(int,input().split()))
+b,c=map(int,input().split())
+res=0
+for i in range(n):
+    n_cnt[i]-=b
+    res+=1
+    if n_cnt[i]>0:
+        if n_cnt[i]%c==0:
+            res+=(n_cnt[i]//c)
+        else:
+            res+=(n_cnt[i]//c+1)
+
+print(res)
